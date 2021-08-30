bc8b97f061008cd18a35042c17df16bba2e0cc6c
gong4857 <gong4857@naver.com>
2021-08-30 16:52:20 +0900

    초기 커밋.

---
+++ if문/9498.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int A;
+	scanf("%d", &A);
+
+	if (A > 89)
+	{
+		printf("A");
+	}
+	else if (A > 79)
+	{
+		printf("B");
+	}
+	else if (A > 69)
+	{
+		printf("C");
+	}
+	else if (A > 59)
+	{
+		printf("D");
+	}
+	else
+	{
+		printf("F");
+	}
+
+
+	return 0;
+}
