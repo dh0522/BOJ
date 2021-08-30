73b9bd4741c8039f234c9bffacf6409e104ff520
gong4857 <gong4857@naver.com>
2021-08-30 16:54:20 +0900

    bj

---
+++ if문/1330.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main2()
+{
+
+	int A, B;
+	scanf("%d %d", &A, &B);
+
+	if (A > B)
+	{
+		printf(">");
+	}
+	else if (A < B)
+	{
+		printf("<");
+	}
+	else
+	{
+		printf("==");
+	}
+
+
+	return 0;
+}
