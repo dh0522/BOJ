dcef700a4df4b9346334ac848d1a37c32c9edc2f
gong4857 <gong4857@naver.com>
2021-09-01 19:53:09 +0900

    210901 다시

--- 1차원 배열/10818.c
+++ 1차원 배열/10818.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
 int main() {
-	int n;
-
+	int n;//n=정수의 갯수
+	int num;
+	int max = -1000000, min = 10000000;
 	scanf("%d", &n);
 
+	int i;
+	for (i = 0; i < n; i++) {
+		scanf("%d", &num);
+		if (num < min)	min = num;
+		if (num > max) max = num;
 
+	}
+	printf("%d %d\n", min, max);
+	return 0;
 }
